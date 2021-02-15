27331b0762d1871765d8b56acfa01844dd91cf52
benedict <kiokobenedict@gmail.com>
2021-02-15 20:02:52 +0300

    connecting react to apollo

--- client/src/index.js
+++ client/src/index.js
@@ -6,10 +6,12 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import App from "./App";
 import "./index.css";
 
+// URL to use when fetching data
 const client = new ApolloClient({
   uri: "http://localhost:4000",
 });
 
+// Provider expects to receive the client object to be able to connect Apollo to React
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
